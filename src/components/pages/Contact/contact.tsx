be4149392197e5d265727280ba860b0aaa365423
Richard-Persson <ricpersson00@gmail.com>
2024-10-17 10:16:15 +0200

    La til Contact side

---
+++ src/components/pages/Contact/contact.tsx
@@ -0,0 +1,26 @@
+
+import Header from '../../header/Header.tsx'
+
+
+export function Contact(){
+
+
+
+
+    return(<>
+
+        <Header/>
+
+        <form>
+        <label> Name 
+        <input type="text"/>
+        </label>
+
+
+        </form>
+
+
+    
+    </>)
+}
+
